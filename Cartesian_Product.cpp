cd758270f5ababb1f97ca91b167af09a16645baa
shadow-island <uudoin@gmail.com>
2016-10-29 01:49:26 -0600

    code meaningful

--- Cartesian_Product.cpp
+++ Cartesian_Product.cpp
@@ -5,36 +5,36 @@ using namespace std;
 
 template<typename T>
 
-vector<vector<T>> cart_product (const vector<vector<T>>& v) 
+vector<vector<T>> cart_product (const vector<vector<T>> input) 
 {
-	vector<vector<T>> s = {{}};
-	for (auto& u : v) 
+	vector<vector<T>> output = {{}};
+	for (auto layer : input) 
 	{
-		vector<vector<T>> r;
-		for (auto& x : s) 
+		vector<vector<T>> temp_output;
+		for (auto x : output) 
 		{
-			for (auto y : u) 
+			for (auto col : layer) 
 			{
-				r.push_back(x);
-				r.back().push_back(y);
+				temp_output.push_back(x);
+				temp_output.back().push_back(col);
 			}
 		}
-	s.swap(r);
+		output.swap(temp_output);
 	}
-	return s;
+	return output;
 } 
 
 int main()
 {
-    vector<vector<int>> test{{2,5,4}, {3,7,8,9}, {5,5,7,8,9,10}};
+    vector<vector<int>> test{{1,2}, {1,2,3}, {1,2}};
     vector<vector<int>> cartprodResult; // result
     cartprodResult = cart_product(test); // Cartesian Product
 
-    for (auto& x : cartprodResult)
+    for (auto row : cartprodResult)
     {
-        for (auto y : x)
+        for (auto col : row)
         {
-            cout << y << "\t";
+            cout << col << "\t";
         }
         cout << endl;
     }
